12eb07c318727c3e50d7d827fec489c7a5720b04
Famille Manai <famillemanai@iMac-de-Famille.local>
2023-11-20 12:09:13 -0500

    Modify card presentation

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ fetch('https://imdb-com.p.rapidapi.com/auto-complete?q=game', {
         'X-RapidAPI-Host': 'imdb-com.p.rapidapi.com'
     }
 })
-.then(response => response.json())
-.then(responseData => {
-    const list = responseData.data.d;
-    list.map((item, index) => {
-        // Skip the first item (index 0)
-        if (index === 0) return;
+    .then(response => response.json())
+    .then(responseData => {
+        const list = responseData.data.d;
+        list.map((item, index) => {
+            // Skip the first item (index 0)
+            if (index === 0) return;
 
-        if (item.l && item.i && item.i.imageUrl) {
-            const name = item.l;
-            const poster = item.i.imageUrl;
-            const movie = `<div class="card">
+            if (item.l && item.i && item.i.imageUrl) {
+                const name = item.l;
+                const poster = item.i.imageUrl;
+                const movie = `<div class="card">
                             <div class="content-card">
                                 <img 
                                     src="${poster}" alt="..."
@@ -23,32 +23,32 @@ fetch('https://imdb-com.p.rapidapi.com/auto-complete?q=game', {
                             </div>
                         </div>`;
 
-            document.querySelector(".movies").innerHTML += movie;
-        }
-        const card = document.querySelectorAll(".card");
+                document.querySelector(".movies").innerHTML += movie;
+            }
+            const card = document.querySelectorAll(".card");
 
-        card.forEach(el => {
-            el.addEventListener("mousemove", e => {
-                let elRect = el.getBoundingClientRect();
-                console.log(e)
-                let x = e.clientX - elRect.x;
-                let y = e.clientY - elRect.y;
-        
-                let midCardWidth = elRect.width / 2;
-                let midCardHeight = elRect.height / 2;
-        
-                let angleY = -(x - midCardWidth) / 8;
-                let angleX = (y - midCardHeight) / 8;
-        
-                el.children[0].style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`;
+            card.forEach(el => {
+                el.addEventListener("mousemove", e => {
+                    let elRect = el.getBoundingClientRect();
+                    console.log(e)
+                    let x = e.clientX - elRect.x;
+                    let y = e.clientY - elRect.y;
+
+                    let midCardWidth = elRect.width / 2;
+                    let midCardHeight = elRect.height / 2;
+
+                    let angleY = -(x - midCardWidth) / 8;
+                    let angleX = (y - midCardHeight) / 8;
+
+                    el.children[0].style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`;
+                });
+
+                el.addEventListener("mouseleave", () => {
+                    el.children[0].style.transform = `rotateX(0) rotateY(0)`;
+                })
             });
-
-            el.addEventListener("mouseleave", () => {
-                el.children[0].style.transform = `rotateX(0) rotateY(0)`;
-            })
         });
+    })
+    .catch(err => {
+        console.error(err);
     });
-})
-.catch(err => {
-    console.error(err);
-});
